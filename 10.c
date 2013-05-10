485f9d400cce5de56cf4bbb8b62129b8fe3f852d
James Baumgarten <jebaum@ucla.edu>
2013-05-10 02:52:22 -0700

    optimized sieve in #10

--- 10.c
+++ 10.c
@@ -7,30 +7,31 @@
 #include <math.h>
 #define UPPER_LIMIT 2000000
 
+// using index math described in 010_overview.pdf to optimize sieve
 int main()
 {
-  char primes[UPPER_LIMIT];
+  char primes[(UPPER_LIMIT-1)/2];
   int i,j;
   double sum = 2;
 
-  for (i=3; i<UPPER_LIMIT; ++i) // initialize sieve
+  for (i=1; i<(UPPER_LIMIT-1)/2; ++i) // initialize sieve
     primes[i] = 'p';
 
-  for (i=3; i <= sqrt(UPPER_LIMIT); i+=2) // mark nonprimes with 'x'
+  for (i=1; i <= (sqrt(UPPER_LIMIT)-1)/2; ++i) // mark nonprimes with 'x'
   {
     if (primes[i] == 'p')
     {
-      for (j=i*i; j<UPPER_LIMIT; j+=i)
+      for (j=2*i*(i+1); j<(UPPER_LIMIT-1)/2; j+=(2*i+1))
       {
         primes[j] = 'x';
       }
     }
   }
 
-  for (i=3; i<UPPER_LIMIT; i+=2)
+  for (i=1; i<(UPPER_LIMIT-1)/2; ++i)
   {
     if (primes[i] == 'p')
-      sum+=(double)i; // I couldn't get unsigned long long ints to work, and floats weren't precise enough
+      sum+=(double)(2*i+1); // I couldn't get unsigned long long ints to work, and floats weren't precise enough
   }
   printf("sum is %f\n", sum);
 }
