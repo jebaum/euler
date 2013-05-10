5ae37b14e9dcf78f5060ffa537628aa04044fe2d
James Baumgarten <jebaum@ucla.edu>
2013-05-09 23:33:40 -0700

    solved #9

---
+++ 9.c
@@ -0,0 +1,34 @@
+/* A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^2 + b^2 = c^2
+ *
+ * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
+// a+b>c, so c<=499
+// b > a
+
+#include <stdio.h>
+
+int main()
+{
+  int a, b, c;
+
+  for (a=1; a<=1000; ++a)
+  {
+    for (b=a+1; b<=1000-a; ++b)
+    {
+      c = 1000 - a - b;
+      if ( (a*a + b*b == c*c) && a<b )
+      {
+        printf("a * b * c = %d * %d * %d = %d\n", a, b, c, a*b*c);
+        break;
+      }
+    }
+    if (b > 1001-a)
+      break;
+  }
+  return 1;
+}
