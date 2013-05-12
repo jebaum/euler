0cc9374fcd1eda309efa2a5850715fc3152a018e
James Baumgarten <jebaum@ucla.edu>
2013-05-12 13:46:43 -0700

    remove pdfs, fix small bug in 010.c

--- 010.c
+++ 010.c
@@ -10,29 +10,22 @@
 // using index math described in 010_overview.pdf to optimize sieve
 int main()
 {
-  char primes[(UPPER_LIMIT-1)/2];
+  char primes[(UPPER_LIMIT+1)/2];
   int i,j;
   unsigned long long sum = 2;
 
-  for (i=1; i<(UPPER_LIMIT-1)/2; ++i) // initialize sieve
+  for (i=1; i<(UPPER_LIMIT+1)/2; ++i) // initialize sieve
     primes[i] = 'p';
 
-  for (i=1; i <= (sqrt(UPPER_LIMIT)-1)/2; ++i) // mark nonprimes with 'x'
-  {
+  for (i=1; i <= (sqrt(UPPER_LIMIT)+1)/2; ++i) // mark nonprimes with 'x'
     if (primes[i] == 'p')
-    {
-      for (j=2*i*(i+1); j<(UPPER_LIMIT-1)/2; j+=(2*i+1))
-      {
+      for (j=2*i*(i+1); j<(UPPER_LIMIT+1)/2; j+=(2*i+1))
         primes[j] = 'x';
-      }
-    }
-  }
 
-  for (i=1; i<(UPPER_LIMIT-1)/2; ++i)
-  {
+  for (i=1; i<(UPPER_LIMIT+1)/2; ++i) // sum them up
     if (primes[i] == 'p')
-      sum+=(2*i+1);
-  }
+      sum+= 2*i+1;
+
   printf("sum is %llu\n", sum);
 }
 
